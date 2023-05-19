67d71dd304be4ffcde6e31e50b7da9224a913ca7
2!ZM$ftvapiA <freesoftwaredave@gmail.com>
2023-05-19 11:02:21 -0700

    Review before deployment

--- DeployDocker.sh
+++ DeployDocker.sh
@@ -1,22 +1,4 @@
 !#/bin/bash
-#
-echo
-    read -p -r "Run update/upgrade cycle?  (Yy/Nn) " yn
-    case $yn in
-        [Yy] ) echo "Excellent!";
-            apt update;
-            apt upgrade -y;;
-        [Nn] ) echo "Alright, lets try again";
-            echo;
-            echo;;
-    esac
-
-
-
-
-
-
-    #!/bin/bash
 
 #----------------------------------------------------------------
 #---    Script explanation
@@ -29,6 +11,18 @@ echo
 echo "Begin Script"
 echo
 echo
+echo
+    read -p -r "Run update/upgrade cycle?  (Yy/Nn) " yn
+    case $yn in
+        [Yy] ) echo "Excellent!";
+            apt update;
+            apt upgrade -y;;
+        [Nn] ) echo "Alright, lets try again";
+            echo;
+            echo;;
+    esac
+#---
+
 
 #----------------------------------------------------------------
 #--- Update/Upgrade cycle?  Reboot?
@@ -51,27 +45,25 @@ read -p -r "Reboot?  (Yy/Nn) " yn
     esac
 #---
 
+
 #----------------------------------------------------------------
-#---    Start with installing from vendor repos
+#---    Start by removing any previous installed components
 #---
 #---
 sudo apt-get -y remove docker docker-engine docker.io containerd runc
 sudo apt update
-sudo apt install apt-transport-https ca-certificates curl gnupg lsb-release
+sudo apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
+
 
 #----------------------------------------------------------------
 #---    Set up secure docker repositories
 #---
 #---
-sudo apt-get -y remove docker docker-engine docker.io containerd runc
-
-sudo apt-get update
 sudo apt-get -y install ca-certificates curl gnupg
-
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
-
-echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose docker-compose-plugin
 
