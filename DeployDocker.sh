470b68dd39b94f4ca6e3c466ce4e51c9fdda9f00
Dave Thompsom <32268058+OS-DaveThompson@users.noreply.github.com>
2023-05-26 00:25:25 -0700

    Reinforced methodology
    *Rewrite of a lot of the script to provide consistency in the script, but also presentation to the user.  
    *Also removed redundant code, re-organized the flow minutely.
    
    *Methodology:
    This script is designed for learning, so there should be lots of comments and debug messages.  And lean towards verbosity.

--- DeployDocker.sh
+++ DeployDocker.sh
@@ -12,61 +12,86 @@ echo "Begin Script"
 echo
 echo
 echo
-    read -p -r "Run update/upgrade cycle?  (Yy/Nn) " yn
+#----------------------------------------------------------------
+#--- Update/Upgrade cycle?
+#---
+read -p "Run update/upgrade cycle?  (Yy/Nn) " yN
     case $yn in
-        [Yy] ) echo "Excellent!";
+        [Yy] ) echo;
+            echo "Selected: Perform update";
+            echo;
             apt update;
             apt upgrade -y;;
-        [Nn] ) echo "Alright, lets try again";
-            echo;
+         * ) echo;
+            echo "Selected: No update";
             echo;;
     esac
-#---
-
-
+#================================================================
 
 #----------------------------------------------------------------
-#--- Update/Upgrade cycle?  Reboot?
+#--- Reboot?
 #---
-read -p -r "Run update/upgrade cycle?  (Yy/Nn) " yn
+read -p "Reboot?  (Yy/Nn) " yN
     case $yn in
-        [Yy] ) echo "Updating!";
-            apt update;
-            apt upgrade -y;;
-        [Nn] ) echo "Alright, no updates!";
+        [Yy] ) echo;
+            echo "Selected: Rebooting";
             echo;
-            echo;;
-    esac
-read -p -r "Reboot?  (Yy/Nn) " yn
-    case $yn in
-        [Yy] ) echo "Rebooting!";
             shutdown -r now;;
-        [Nn] ) echo "Alright, not rebooting!";
+         * ) echo; 
+            echo "Selected: No reboot";
             echo;;
     esac
-#---
-
-
+#================================================================
 
 #----------------------------------------------------------------
-#---    Start by removing any previous installed components
+#--- Start by removing any previous installed docker components
 #---
 #---
+echo
+echo "#================================================================"
+echo "# 1) Remove prior docker and related components"
+echo "# 2) Perform Update to refresh app-cache"
+echo "# 3) Install latest secure transport crypto and related functions"
+echo "#================================================================"
+echo
 sudo apt-get -y remove docker docker-engine docker.io containerd runc
 sudo apt update
 sudo apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
-
-
+#================================================================
 
 #----------------------------------------------------------------
-#---    Set up secure docker repositories
+#--- Set up secure docker repositories
 #---
 #---
-sudo apt-get -y install ca-certificates curl gnupg
+echo
+echo "#================================================================"
+echo "# 1) Install crypto keyring"
+echo "# 2) Download docker key and put it into the keyring"
+echo "# 3) Secure permissions on docker key"
+echo "# 4) Create repository referense"
+echo "#================================================================"
+echo
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+#================================================================
+
+#----------------------------------------------------------------
+#--- Install docker and related components
+#---
+#---
+echo
+echo "#================================================================"
+echo "# 1) Perform update to refresh app-cache"
+echo "# 2) APT install docker and components"
+echo "#================================================================"
+echo
 apt update
 apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose docker-compose-plugin
-
+#================================================================
+echo
+echo
+echo "docker --version"
+echo "#---"
+docker --version
